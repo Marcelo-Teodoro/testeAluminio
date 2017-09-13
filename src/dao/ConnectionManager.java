a9b21c813168980914b6b0494cdd264f0e6ee419
Marcelo-Teodoro <darkskullwow@gmail.com>
2017-09-13 18:20:56 -0300

    teste

--- src/dao/ConnectionManager.java
+++ src/dao/ConnectionManager.java
@@ -11,9 +11,13 @@ public class ConnectionManager {
         Connection conn = null;
 
         String STR_DRIVER = "org.gjt.mm.mysql.Driver";
-        String STR_CONEX = "jdbc:mysql://localhost:3306/calendariodb";
-        String USER = "root";
-        String PASSWORD = "root";
+//        String STR_CONEX = "jdbc:mysql://localhost:3306/calendariodb";
+//        String USER = "root";
+//        String PASSWORD = "root";
+
+        String STR_CONEX = "jdbc:mysql://databases-auth.000webhost.com/db_structure.php?server=1&db=id2890822_testealuminio&token=4e1239693c0820065ce9856179fdc96c";
+        String USER = "id2890822_marcelo_adonis";
+        String PASSWORD = "teste";
 
 
         Class.forName(STR_DRIVER);
