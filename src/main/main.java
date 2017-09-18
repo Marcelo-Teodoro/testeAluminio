c0ae1b0f1bac7a8245023bdca424623866c56b2f
Marcelo-Teodoro <darkskullwow@gmail.com>
2017-09-18 15:56:30 -0300

    botao refeito

--- src/main/main.java
+++ src/main/main.java
@@ -1,13 +1,15 @@
 package main;
 
+import view.PrimeiroPanel;
+
 import javax.swing.*;
 
 public class main {
 
     public static void main(String[] args) {
-        JFrame j = new JFrame();
-        j.setVisible(true);
-        j.setSize(800,600);
-        j.setLocationRelativeTo(null);
+        PrimeiroPanel pp = new PrimeiroPanel();
+//        pp.setV();
+//        pp.setSize(800,600);
+//        pp.setLocationRelativeTo(null);
     }
 }
--- src/view/PrimeiroPanel.java
+++ src/view/PrimeiroPanel.java
@@ -4,8 +4,66 @@ import javax.swing.*;
 
 public class PrimeiroPanel {
     private JPanel panel1;
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane tbdPane;
     private JButton btnPerfil;
+    private JButton btnAcessorio;
+    private JButton btnEsquadria;
+    private JPanel jPanelTab;
+    private JButton btnContato;
 
+    public JPanel getPanel1() {
+        return panel1;
+    }
 
+    public void setPanel1(JPanel panel1) {
+        this.panel1 = panel1;
+    }
+
+    public JTabbedPane getTbdPane() {
+        return tbdPane;
+    }
+
+    public void setTbdPane(JTabbedPane tbdPane) {
+        this.tbdPane = tbdPane;
+    }
+
+    public JButton getBtnPerfil() {
+        return btnPerfil;
+    }
+
+    public void setBtnPerfil(JButton btnPerfil) {
+        this.btnPerfil = btnPerfil;
+    }
+
+    public JButton getBtnAcessorio() {
+        return btnAcessorio;
+    }
+
+    public void setBtnAcessorio(JButton btnAcessorio) {
+        this.btnAcessorio = btnAcessorio;
+    }
+
+    public JButton getBtnEsquadria() {
+        return btnEsquadria;
+    }
+
+    public void setBtnEsquadria(JButton btnEsquadria) {
+        this.btnEsquadria = btnEsquadria;
+    }
+
+    public JPanel getjPanelTab() {
+        return jPanelTab;
+    }
+
+    public void setjPanelTab(JPanel jPanelTab) {
+        this.jPanelTab = jPanelTab;
+    }
+
+    public JButton getBtnContato() {
+        return btnContato;
+    }
+
+    public void setBtnContato(JButton btnContato) {
+        this.btnContato = btnContato;
+    }
 }
