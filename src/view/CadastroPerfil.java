bb0327c41210ecfb82367773cc872ece449be2fb
Marcelo-Teodoro <darkskullwow@gmail.com>
2017-09-18 17:29:41 -0300

    icone

---
+++ src/view/CadastroPerfil.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class CadastroPerfil {
+    private JPanel perfilFundoPanel;
+    private JLabel fornecedorLabel;
+    private JTextField textField1;
+    private JLabel tipoLabel;
+    private JTextField textField2;
+}
